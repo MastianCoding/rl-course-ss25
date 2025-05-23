09894ee28b14c96556a270501d64a4c0ed6bbce2
MastianCoding <zettler.maximilian@gmx.de>
2025-05-23 12:37:49 +0200

    tmp

--- 1_FrozenLake_Random.py
+++ 1_FrozenLake_Random.py
@@ -10,25 +10,23 @@ print("## Frozen Lake ##")
 action2string = {0: "Left", 1: "Down", 2: "Right", 3: "Up"}
 reward = 0
 counter = 0
-successful_path = []
-while reward <1:
-    episode_done = False
-    state = env.reset(seed=0)
+path = [(0,1), (4, 1), (8, 1), (9, 2), (8, 0), (4, 3), (0, 3), (0, 0), (4, 1), (8, 1), (9, 2), (10, 2), (14, 1), (14, 1), (15, 2)]
+episode_done = False
+state = env.reset(seed=0)
 
-    print("Start state:")
+print("Start state:")
+print(env.render())
+counter += 1
+while not episode_done:
+    test_state, action = path.pop(0)
+    #assert test_state == state or state.
+    state, reward, episode_done, _, _ = env.step(action)
+    print(f"\nAction:{action2string[action]}, new state:{state}, reward:{reward}")
     print(env.render())
-    path = []
-    counter += 1
-    while not episode_done:
-        action = random.randint(0, 3)  # choose a random action
-        state, reward, episode_done, _, _ = env.step(action)
-        print(f"\nAction:{action2string[action]}, new state:{state}, reward:{reward}")
-        print(env.render())
-        path.append((state, action))
-        if reward > 0:
-            successful_path = path
+    path.append((state, action))
 
 print("Counter: {}".format(counter))
 #Counter: 170
-print("Successful path: {}".format(successful_path))
-#Successful path: [(4, 1), (8, 1), (9, 2), (8, 0), (4, 3), (0, 3), (0, 0), (4, 1), (8, 1), (9, 2), (10, 2), (14, 1), (14, 1), (15, 2)]
+#print("Successful path: {}".format(successful_path))
+# von Hand hinzugefügt von Start nach unten zu gehen)
+#Successful path: [(0,1), (4, 1), (8, 1), (9, 2), (8, 0), (4, 3), (0, 3), (0, 0), (4, 1), (8, 1), (9, 2), (10, 2), (14, 1), (14, 1), (15, 2)]
